e47b6bfa278e21957b86c316fca7c9ebdc4e8b41
cmoore187 <calebmoore187@gmail.com>
2021-05-08 14:10:29 -0400

    Updated files

---
+++ homework.sql
@@ -0,0 +1,33 @@
+-- create the tables
+--titles
+CREATE TABLE titles(
+	title_id VARCHAR PRIMARY KEY,
+	titles VARCHAR
+);
+--employees
+CREATE TABLE employees(
+	emp_no INT PRIMARY KEY,
+	emp_title_id VARCHAR,
+	birth_date DATE,
+	first_name VARCHAR,
+	last_name VARCHAR,
+	sex VARCHAR,
+	hire_data DATE
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+--departments
+CREATE TABLE departments(
+	dept_no VARCHAR PRIMARY KEY,
+	dept_name VARCHAR
+);
+--department managers
+CREATE TABLE dept_managers(
+	dept_no VARCHAR,
+	emp_no INT,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY(dept_no, emp_no)
+
+);
+--department employees
+--salaries
